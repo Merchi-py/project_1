9f26abbb31cf7c9ee8c041e181d86cd693576fb3
Svyat <svyat.kostevich@gmail.com>
2024-12-18 17:10:25 +0100

    added(option.js) added ajax price system

--- static/js/option.js
+++ static/js/option.js
@@ -1,1 +1,19 @@
-$("#")
+$(".btn-price").click(function(){
+    let btn = $(this)
+        $.ajax("/", {
+        "type": "POST",
+        "async": true,
+        "dataType": json,
+        "data":{
+            "price": btn.data("price")
+        };
+        "success": function(response){
+        document.getElementById("#price-tours").innerhtml = `${response.picture}`
+        document.getElementById("#price-tours").innerhtml = `${response.name}`
+        document.getElementById("#price-tours").innerhtml = `${response.price}`
+        document.getElementById("#price-tours").innerhtml = `${response.description}`
+        document.getElementById("#price-tours").innerhtml = `${response.people}`
+        document.getElementById("#price-tours").innerhtml = `${response.time}`
+        }
+    })
+})
