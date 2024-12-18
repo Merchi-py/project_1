c02b45500a59d0e912449e99868db17abca68d66
Svyat <svyat.kostevich@gmail.com>
2024-12-18 17:08:34 +0100

    added(book.js) update views with new pages like edit_tour and delt_tour

--- views.py
+++ views.py
@@ -1,16 +1,34 @@
+from sqlalchemy.orm import session
+
 from settings import *
 from db import *
 from fastapi import Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+
+def login_requiered(endpoint):
+    def wrapper(request, *args, **kwargs):
+        if not request.session.get("is_authentificated", False):
+            return RedirectResponse("/login", status_code=303)
+        endpoint(request, *args, **kwargs)
+        return wrapper()
+
 #main_page
+@app.post("/")
+async def main_post(request: Request, price: int = Form(), db: Session = Depends(get_db)):
+    price_tours = db.query(Tour).filter_by(price=price)
+    tours = ""
+    for t in price_tours:
+        tours += f"{t.name}{t.price}{t.description}{t.people}{t.time}{t.picture}"
+    return {"tours": tours}
+
 @app.get("/", response_class=HTMLResponse)
-async def main(request: Request, db: Session = Depends(get_db)):
-    tours = db.query(Tour).all()
+async def main(request: Request, price: int = Form(), db: Session = Depends(get_db)):
+    tour = db.query(Tour).all()
     user = db.query(User).get(id=id)
-    return templates.TemplateResponse("main.html", {"request": request, "tours": tours, "user": user})
+    return templates.TemplateResponse("main.html", {"request": request, "tour": tour, "user": user})
 
-
+# user_registration_page
 @app.get('/registration', response_class=HTMLResponse)
 def login(request: Request):
     return templates.TemplateResponse('registration.html', {"request": request})
@@ -52,10 +70,8 @@ def logout(request: Request):
     request.session['is_auth'] = False
     return RedirectResponse('/', status_code=303)
 
-# admin page
-
 # add tour
-
+@login_requiered
 @app.get("/add_tour", response_class=HTMLResponse)
 async def add_tour_page(request: Request):
     return templates.TemplateResponse("/add_tour.html", {"request": request})
@@ -74,14 +90,15 @@ async def add_tour(name: str = Form(), price: int = Form(), description: str = F
 
 
 #edit tour
-
-
-
+@login_requiered
 @app.get("/edit_tour/{tour.id}", response_class=HTMLResponse)
-async def edit_tour_page(request: Request):
+async def edit_tour_page(id, request: Request, db: Session = Depends(get_db)):
+    tour = db.query(Tour).get(id=id)
     return templates.TemplateResponse("/edit_tour.html", {"request": request})
+
+
 @app.post("/edit_tour/{tour.id}")
-async def edit_tour(name: str = Form(), price: int = Form(), description: str = Form(), time: str = Form(), people: int = Form(), db: Session = Depends(get_db)):
+async def edit_tour(id, name: str = Form(), price: int = Form(), description: str = Form(), time: str = Form(), people: int = Form(), db: Session = Depends(get_db)):
     tours = db.query(Tour).get(id=id)
     datetime.strptime(time, '%d.%m.%Y') or datetime.strptime(time, '%Y.%m.%d')
     tours.name = name
@@ -96,10 +113,26 @@ async def edit_tour(name: str = Form(), price: int = Form(), description: str = 
     return RedirectResponse("/", status_code=303)
 
 
-
 # delete tour
-# @app.post("/delete_tour/{tour_id}")
-# async def delete_tour(db: Session = Depends(get_db)):
-#     tour = db.query(Tour).get(id=id)
-#     del tour
-#     return RedirectResponse("/", status_code=303)
+@app.post("/delete_tour/{tour_id}")
+async def delete_tour(id, db: Session = Depends(get_db)):
+    tour = db.query(Tour).get(id=id)
+    del tour
+    return RedirectResponse("/", status_code=303)
+
+@login_requiered
+@app.post("/add_basket/{tour.id}")
+async def add_basket(id):
+    if not session.get("basket"):
+        session["basket"] = []
+    session["basket"].append(id)
+    return RedirectResponse("/", status_code=303)
+
+@app.post("/basket")
+async def basket(request: Request, db: Session = Depends(get_db)):
+    if not session.get("basket"):
+        session["basket"] = []
+    bask = []
+    for id in session["basket"]:
+        bask.append(db.query(Tour).get(id))
+    return {"request": request, "bask": bask}
