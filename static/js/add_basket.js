aabf8b6d37262a966636efab193ff7aa4fb69fb5
Svyat <svyat.kostevich@gmail.com>
2024-12-25 14:01:59 +0100

    add(add_basket.js): added ajax request for button add_basket

---
+++ static/js/add_basket.js
@@ -0,0 +1,10 @@
+$("#add_to_basket").click(function(){
+    var btn = $(this);
+        $.ajax(btn.data("url"), {
+        "type": "POST",
+        "async": true,
+        "dataType": "json",
+        "data": {
+        }
+    });
+})
