d16b34d2d901495c37600692d5f6b34503215c5f
Svyat <svyat.kostevich@gmail.com>
2024-12-25 13:55:04 +0100

    add(views): added basket and buy functions for models

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import session
 
 from settings import *
 from db import *
-from fastapi import Request, Form, Depends
+from fastapi import Request, Form, Depends, UploadFile, File
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 
@@ -15,18 +15,19 @@ def login_requiered(endpoint):
 
 #main_page
 @app.post("/")
-async def main_post(request: Request, price: int = Form(), db: Session = Depends(get_db)):
+async def main_post(price: int = Form(), db: Session = Depends(get_db)):
     price_tours = db.query(Tour).filter_by(price=price)
     tours = ""
     for t in price_tours:
-        tours += f"{t.name}{t.price}{t.description}{t.people}{t.time}{t.picture}"
+        tours += f"{t.name} {t.price} {t.description} {t.people} {t.time} {t.picture}"
     return {"tours": tours}
 
+@login_requiered
 @app.get("/", response_class=HTMLResponse)
-async def main(request: Request, price: int = Form(), db: Session = Depends(get_db)):
-    tour = db.query(Tour).all()
-    user = db.query(User).get(id=id)
-    return templates.TemplateResponse("main.html", {"request": request, "tour": tour, "user": user})
+async def main(request: Request, db: Session = Depends(get_db)):
+    tours = db.query(Tour).all()
+    user = db.query(User).get(request.session["user_id"])
+    return templates.TemplateResponse("main.html", {"request": request, "tours": tours, "user": user})
 
 # user_registration_page
 @app.get('/registration', response_class=HTMLResponse)
@@ -77,16 +78,19 @@ async def add_tour_page(request: Request):
     return templates.TemplateResponse("/add_tour.html", {"request": request})
 
 
+@login_requiered
 @app.post("/add_tour")
-async def add_tour(name: str = Form(), price: int = Form(), description: str = Form(), time: str = Form(), people: int = Form() , db: Session = Depends(get_db)):
-    user = db.query(User).filter_by(name=name).first()
+async def add_tour(request: Request, name: str = Form(), price: int = Form(), description: str = Form(), from_time: str = Form(), to_time: str = Form(), people: int = Form(), db: Session = Depends(get_db)):
+    user = db.query(User).get(request.session["user_id"])
     if not user.is_admin:
         return templates.TemplateResponse("/admin.html", {"message": "Wrong password"})
-    datetime.strptime(time, '%d.%m.%Y') or datetime.strptime(time, '%Y.%m.%d')
-    tours = Tour(name=name, price=price, description=description, time=time, people=people)
+    from_time = datetime.strptime(from_time, '%Y-%m-%d')
+    to_time = datetime.strptime(to_time, '%Y-%m-%d')
+    tours = Tour(name=name, price=price, description=description, from_time=from_time, to_time=to_time, people=people)
     db.add(tours)
     db.commit()
     db.refresh(tours)
+    return RedirectResponse("/", status_code=303)
 
 
 #edit tour
@@ -97,6 +101,7 @@ async def edit_tour_page(id, request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse("/edit_tour.html", {"request": request})
 
 
+@login_requiered
 @app.post("/edit_tour/{tour.id}")
 async def edit_tour(id, name: str = Form(), price: int = Form(), description: str = Form(), time: str = Form(), people: int = Form(), db: Session = Depends(get_db)):
     tours = db.query(Tour).get(id=id)
@@ -113,26 +118,41 @@ async def edit_tour(id, name: str = Form(), price: int = Form(), description: st
     return RedirectResponse("/", status_code=303)
 
 
+@login_requiered
 # delete tour
 @app.post("/delete_tour/{tour_id}")
-async def delete_tour(id, db: Session = Depends(get_db)):
-    tour = db.query(Tour).get(id=id)
+async def delete_tour(tour_id: int, db: Session = Depends(get_db)):
+    tour = db.query(Tour).get(tour_id)
     del tour
     return RedirectResponse("/", status_code=303)
 
 @login_requiered
-@app.post("/add_basket/{tour.id}")
-async def add_basket(id):
-    if not session.get("basket"):
-        session["basket"] = []
-    session["basket"].append(id)
+@app.post("/add_basket/{id}")
+async def add_basket(id: int, request: Request):
+    if not request.session.get("basket"):
+        request.session["basket"] = []
+    request.session["basket"].append(id)
     return RedirectResponse("/", status_code=303)
 
+@login_requiered
 @app.post("/basket")
 async def basket(request: Request, db: Session = Depends(get_db)):
-    if not session.get("basket"):
-        session["basket"] = []
-    bask = []
-    for id in session["basket"]:
-        bask.append(db.query(Tour).get(id))
-    return {"request": request, "bask": bask}
+    if not request.session.get("basket"):
+        request.session["basket"] = []
+    basket = ""
+    for b in request.session["basket"]:
+        tour = db.query(Tour).get(b)
+        basket += f"{tour.name} {tour.price}"
+    return {"basket": basket}
+
+
+@login_requiered
+@app.post("/buy")
+async def buy(request: Request, db: Session = Depends((get_db))):
+    user = db.query(User).get(request.session["user_id"])
+    for b in request.session["basket"]:
+        tours = db.query(Tour).get(b)
+        if user.balance <= tours.price:
+            message = "Not enough money"
+            return {"message": message}
+    return {}
