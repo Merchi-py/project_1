ad7d8ba6336f9f8e17ba9f651f86cec74ac3ed77
Svyat <svyat.kostevich@gmail.com>
2024-12-25 14:00:01 +0100

    add(option.js): add ajax request for price options

--- static/js/option.js
+++ static/js/option.js
@@ -3,17 +3,13 @@ $(".btn-price").click(function(){
         $.ajax("/", {
         "type": "POST",
         "async": true,
-        "dataType": json,
+        "dataType": "json",
         "data":{
             "price": btn.data("price")
-        };
+        },
         "success": function(response){
-        document.getElementById("#price-tours").innerhtml = `${response.picture}`
-        document.getElementById("#price-tours").innerhtml = `${response.name}`
-        document.getElementById("#price-tours").innerhtml = `${response.price}`
-        document.getElementById("#price-tours").innerhtml = `${response.description}`
-        document.getElementById("#price-tours").innerhtml = `${response.people}`
-        document.getElementById("#price-tours").innerhtml = `${response.time}`
+        document.getElementById("price-tours").innerHTML = response.tours
+
         }
-    })
+    });
 })
