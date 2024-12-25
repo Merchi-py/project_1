da29e78adc25b06cba603815edcc30727812239b
Svyat <svyat.kostevich@gmail.com>
2024-12-25 13:56:31 +0100

    add(settings.py): added sessionmiddleware i settings

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
 
 __all__ = ("app", "templates")
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="session123")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
